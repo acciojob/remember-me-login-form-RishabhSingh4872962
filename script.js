822b0e0989a00e446448dfadca082885b0f557a2
RishabhSingh4872962 <109566428+RishabhSingh4872962@users.noreply.github.com>
2024-03-22 11:46:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 //your JS code here. If required.
+const username=document.getElementById("username");
+const password=document.getElementById("password");
+const remember=document.getElementById("checkbox");
+const existingBtn=document.getElementById("existing");
+const btn=document.getElementById("submit");
+function handleClick() {
+	if (remember.checked) {
+		localStorage.setItem("username",username.value)
+		localStorage.setItem("password",password.value);
+		
+	}else if(localStorage.length==2){
+		localStorage.remove("username")
+		localStorage.remove("password");		
+	}
+	if (localStorage.length==2) {
+		existingBtn.hidden=false;
+	}else{
+		existingBtn.hidden=true;
+	}
+	alert("Logged in as");
+}
